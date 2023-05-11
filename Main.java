34c9f65f78db33986d04321650a5f6232de5ceae
Annisaarmaynda <armaynda04@gmail.com>
2023-05-11 16:32:04 +0700

    MENGERJAKAN UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.User;
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
- 
+        
+        Mhs mhs = new Mhs("username", "password");
+
+        mhs.setNama("Annisa Armaynda");
+        mhs.setNim("G.111.20.0047");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("USERNAME: " + mhs.getUsername());
+        System.out.println("PASSWORD: " + mhs.getPassword());
     }
 }
