15520dc9b306289ac8edd33fb710d2f1a53b86e0
Annisaarmaynda <armaynda04@gmail.com>
2023-05-17 14:29:12 +0700

    PERBAIKAN UTS

--- Main.java
+++ Main.java
@@ -4,16 +4,16 @@ import pengguna.Mhs;
 class Main {
     public static void main(String[] args) {
         
-        Mhs mhs = new Mhs("username", "password");
+        Mhs mhs = new Mhs("ini_username", "ini_password");
 
         mhs.setNama("Annisa Armaynda");
         mhs.setNim("G.111.20.0047");
         mhs.setSmt(6);
 
-        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nama Mhs: " + mhs.getNama());
         System.out.println("Nim Mhs: " + mhs.getNim());
         System.out.println("Smt Mhs: " + mhs.getSmt());
-        System.out.println("USERNAME: " + mhs.getUsername());
-        System.out.println("PASSWORD: " + mhs.getPassword());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
     }
 }
